e509f1b17e3013600135455f7588c26ddc460179
Rafael <rafinha_fern@hotmail.com>
2022-05-14 02:18:38 -0300

    logo e primeiras alterações no estilo do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,95 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-
+import {ReactComponent as SearchSvg} from '../assets/img/ion-icons/search-outline.svg';
+import logo from "../assets/img/Magicsportsmini.png"
 function Header () {
     const navigate = useNavigate();
 
     return (
         <Container>
-            <Store>MAGIC sports</Store>
+            <Top>
             <Buttons>
                 <Button onClick={() => navigate("/sign-in")}>Log in</Button>
                 <Button onClick={() => navigate("/sign-up")}>Sign up</Button>
             </Buttons>
+            </Top>
+            <Content>
+            <Img src={logo} alt="logo"/>
+            <SearchDiv>
+            <SearchSvg/>
+            <Search placeholder="Pesquise aqui"/>
+            </SearchDiv>
+            <ButtonsProfile>
+        <Shopping/>
+            </ButtonsProfile>
+            </Content>
         </Container>
     )
 }
 
 export default Header;
 
+const Img = styled.img`
+width:250px;
+height:48px;
+`
 const Container = styled.header`
-    height: 70px;
+    height: 100px;
     width: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background-color: #FDBA22;
-    display: flex;
+    background-color: #33164f;
+    position:relative;
+    display:flex;
+    flex-direction: column;
+    justify-content:space-between;
 `;
-
-const Store = styled.h1`
-`;
-
 const Buttons = styled.div`
 `;
 
 const Button = styled.button`
+width:60px;
+height:30px;
+background-color:#ffff;
+border:none;
+border-radius: 5px;
+margin-left: 10px;
 `;
+const Top = styled.div`
+background-color: #fdb927;
+width:100%;
+height:40px;
+display:flex;
+justify-content:flex-end;
+align-items:center;
+padding: 0 30px;
+box-shadow: 0 2px black;
+`
+const Content = styled.div`
+    width:100%;
+    height:70px;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+`
+const Shopping = styled.button`
+    width:50px;
+    height:10px;
+`
+const ButtonsProfile = styled.div`
+`
+const Search = styled.input`
+    width:300px;
+    height:30px;
+    color:black;
+    border-radius: 4px;
+    padding-left: 36px;
+`
+const SearchDiv = styled.div`
+    position: relative;
+    svg {
+        position:absolute;
+        top:5px;
+        left:5px;
+        width:20px;
+        color:silver;
+    }
+`
